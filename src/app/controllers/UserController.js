09ff749d890d1521d46b5626b6b5628813067f14
Andrei Albrecht <andreialbrecht@gmail.com>
2025-04-09 17:54:10 -0300

    feat: adiciona crud de usuários com bcryptjs, sequelize e yup
    Implementado o CRUD de usuários utilizando:
    - bcryptjs para hash e comparação de senhas
    - sequelize para definição dos models e comunicação com o banco de dados
    - yup para validação dos dados de entrada

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,28 @@
 import UserService from '../services/UserService.js';
 
 export default class UserController {
-  static async getUsers(req, res) {}
-  static async getUserById(req, res) {}
+  static async getUsers(req, res) {
+    try {
+      const users = await UserService.getUsers();
+      return res.status(200).json(users);
+    } catch (error) {
+      if (error.message == 'Nenhum usuário encontrado.') {
+        return res.status(400).json({ error: error.message });
+      }
+      return res.status(500).json({ error: error.message });
+    }
+  }
+  static async getUserById(req, res) {
+    try {
+      const user = await UserService.getUserById(req.params.id);
+      return res.status(200).json(user);
+    } catch (error) {
+      if (error.message == 'Usuário não encontrado.') {
+        return res.status(400).json({ error: error.message });
+      }
+      return res.status(500).json({ error: error.message });
+    }
+  }
   static async createUser(req, res) {
     const { name, email, password } = req.body;
 
@@ -19,6 +39,31 @@ export default class UserController {
       return res.status(500).json({ error: error.message });
     }
   }
-  static async updateUser(req, res) {}
-  static async deleteUser(req, res) {}
+  static async updateUser(req, res) {
+    try {
+      const message = await UserService.updateUser(req.params.id, req.body);
+      return res.status(200).json(message);
+    } catch (error) {
+      if (
+        error.message == 'Usuário não encontrado.' ||
+        error.message == 'Falha na validação.' ||
+        error.message == 'Senha Incorreta' ||
+        error.message == 'Nenhum dado para atualizar foi recebido'
+      ) {
+        return res.status(400).json({ error: error.message });
+      }
+      return res.status(500).json({ error: error.message });
+    }
+  }
+  static async deleteUser(req, res) {
+    try {
+      const message = await UserService.deleteUser(req.params.id);
+      return res.status(200).json(message);
+    } catch (error) {
+      if (error.message == 'Usuário não encontrado.') {
+        return res.status(400).json({ error: error.message });
+      }
+      return res.status(500).json({ error: error.message });
+    }
+  }
 }
--- src/app/services/UserService.js
+++ src/app/services/UserService.js
@@ -3,14 +3,38 @@ import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 
 export default class UserService {
-  static async getUsers() {}
-  static async getUserById() {}
+  static async getUsers() {
+    try {
+      const users = await User.findAll();
+
+      if (users == 0) {
+        throw new Error('Nenhum usuário encontrado.');
+      }
+
+      return users;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+  static async getUserById(id) {
+    try {
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        throw new Error('Usuário não encontrado.');
+      }
+
+      return user;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
   static async createUser(data) {
     try {
       const schema = Yup.object().shape({
         name: Yup.string().required(),
         email: Yup.string().email().required(),
-        password: Yup.string().required().min(8),
+        password: Yup.string().min(8).required(),
       });
 
       if (!(await schema.isValid(data))) {
@@ -42,6 +66,67 @@ export default class UserService {
       throw new Error(error.message);
     }
   }
-  static async updateUser() {}
-  static async deleteUser() {}
+  static async updateUser(id, data) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+        email: Yup.string().email(),
+        newPassword: Yup.string().min(8),
+        oldPassword: Yup.string()
+          .min(8)
+          .when('newPassword', (newPassword, field) =>
+            data.newPassword ? field.required() : field,
+          ),
+      });
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        throw new Error('Usuário não encontrado.');
+      }
+
+      if (!data || Object.keys(data).length === 0) {
+        throw new Error('Nenhum dado para atualizar foi recebido');
+      }
+
+      if (!(await schema.isValid(data))) {
+        throw new Error('Falha na validação.');
+      }
+
+      if (
+        data.oldPassword &&
+        !(await bcrypt.compare(data.oldPassword, user.hashed_password))
+      ) {
+        throw new Error('Senha incorreta.');
+      }
+
+      if (data.newPassword) {
+        const saltRounds = 9;
+        data.hashed_password = await bcrypt.hash(data.newPassword, saltRounds);
+        data.rounds = saltRounds;
+      }
+
+      await user.update(data);
+
+      return { message: 'Usuário atualizado com Sucesso' };
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async deleteUser(id) {
+    try {
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        throw new Error('Usuário não encontrado.');
+      }
+
+      await User.destroy({ where: { id: id } });
+
+      return { message: 'Usuário deletado com sucesso' };
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
